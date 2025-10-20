2ecfe3b590b33f5b65b7cfe279b48a3a13e6c723
Ritchie Ortiz <74946954+xRitchie91@users.noreply.github.com>
2025-10-20 15:20:03 -0700

    updated files
    Completed applicaiton styles, started implementing javascript code

---
+++ index.js
@@ -0,0 +1,66 @@
+let main = {
+    variables: {
+        turn: 'w',
+        selectedpiece: '',
+        highlighted: [],
+        pieces: {
+            w_king: {
+                position: '5_1',
+                img: '&#9812;',
+                captured: false,
+                moved: false,
+                type: 'w_king'
+
+            },
+            w_queen: {
+                position: '5_1',
+                img: '&#9813;',
+                captured: false,
+                moved: false,
+                type: 'w_queen'
+            },
+            w_bishop1: {
+                position: '3_1',
+                img: '&#9815;',
+                captured: false,
+                moved: false,
+                type: 'w_bishop'
+            },
+            w_bishop2: {
+                position: '6_1',
+                img: '&#9815;',
+                captured: false,
+                moved: false,
+                type: 'w_bishop'
+            },
+            w_knight1: {
+                position: '2_1',
+                img: '&#9816;',
+                captured: false,
+                moved: false,
+                type: 'w_knight'
+            },
+            w_knight2: {
+                position: '7_1',
+                img: '&#9816;',
+                captured: false,
+                moved: false,
+                type: 'w_knight'
+            },
+            w_rook1: {
+                position: '1_1',
+                img: '&#9814;',
+                captured: false,
+                moved: false,
+                type: 'w_rook'
+            },
+            w_rook2: {
+                position: '8_1',
+                img: '&#9814;',
+                captured: false,
+                moved: false,
+                type: 'w_rook'
+            },
+        }
+    }
+}
