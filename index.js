aa09358451584f99bee3f4ced7a19841649df695
Ritchie Ortiz <74946954+xRitchie91@users.noreply.github.com>
2025-10-20 15:53:29 -0700

    Updated Index.js
    added cases to javascript code

--- index.js
+++ index.js
@@ -235,4 +235,83 @@ let main = {
     }
   },
 
-  
+  methods: {
+    gamesetup: function() {
+        $('.gamecell').attr('chess', 'null');
+        for (let gamepiece in main.variables.pieces) {
+            $('#' + main.variables.pieces[gamepiece].position).html(main.variables.pieces[gamepiece].img);
+            $('#' + main.variables.pieces[gamepiece].position).attr('chess', gamepiece);
+        }
+    },
+
+    moveoptions: function(selectedpiece) {
+
+        let position = { x: '', y: '' };
+        position.x = main.variables.pieces[selectedpiece].positionsplit('_')[0];
+        position.y = main.variables.pieces[selectedpiece].positionsplit('_')[1];
+
+        // var instead of let
+        var options = [];
+        var coordinates = [];
+        var startpoint = main.variables.pieces[selectedpieces].position;
+        var c1,c2,c3,c4,c5,c6,c7,c8;
+
+        if (main.variables.highlighted.length !=0) {
+            main.methods.togglehighlight(main.variables.highlighted);
+        }
+
+        switch (main.variables.pieces[selectedpiece].type) {
+        case 'w_king':
+
+            if ($('#6_1').attr('chess') == 'null' && $('7_1').attr('chess') == 'null' && main.variables.pieces['w_king'].moved == false &&
+        main.variables.pieces['w_rook2'].moved == false) {
+            coordinates = [{ x: 1, y: 1 },{ x: 1, y: 0 },{ x: 1, y: -1 },{ x: 0, y: -1 },{ x: -1, y: -1 },{ x: -1, y: 0 },{ x: -1, y: 1 },{ x: 0, y: 1 },{x: 2, y: 0}].map(function(val){
+              return (parseInt(position.x) + parseInt(val.x)) + '_' + (parseInt(position.y) + parseInt(val.y));
+            });
+        } else {
+            coordinates = [{ x: 1, y: 1 },{ x: 1, y: 0 },{ x: 1, y: -1 },{ x: 0, y: -1 },{ x: -1, y: -1 },{ x: -1, y: 0 },{ x: -1, y: 1 },{ x: 0, y: 1 }].map(function(val){
+              return (parseInt(position.x) + parseInt(val.x)) + '_' + (parseInt(position.y) + parseInt(val.y));
+            });
+        }
+
+        options = (main.methods.options(startpoint, coordinates, main.variables.pieces[selectedpiece].type)).slice(0);
+        main.variables.highlighted = options.slice(0);
+        main.methods.togglehighlight(options);
+
+        break;
+        
+        case 'b_king':
+
+        if ($('#6_8').attr('chess') == 'null' && $('#7_8').attr('chess') == 'null' && main.variables.pieces['b_king'].moved == false && main.variables.pieces['b_rook2'].moved == false) {
+          coordinates = [{ x: 1, y: 1 },{ x: 1, y: 0 },{ x: 1, y: -1 },{ x: 0, y: -1 },{ x: -1, y: -1 },{ x: -1, y: 0 },{ x: -1, y: 1 },{ x: 0, y: 1 },{x: 2, y: 0}].map(function(val){
+            return (parseInt(position.x) + parseInt(val.x)) + '_' + (parseInt(position.y) + parseInt(val.y));
+          });
+        } else {
+          coordinates = [{ x: 1, y: 1 },{ x: 1, y: 0 },{ x: 1, y: -1 },{ x: 0, y: -1 },{ x: -1, y: -1 },{ x: -1, y: 0 },{ x: -1, y: 1 },{ x: 0, y: 1 }].map(function(val){
+            return (parseInt(position.x) + parseInt(val.x)) + '_' + (parseInt(position.y) + parseInt(val.y));
+          });
+        }
+        /*
+          coordinates = [{ x: 1, y: 1 },{ x: 1, y: 0 },{ x: 1, y: -1 },{ x: 0, y: -1 },{ x: -1, y: -1 },{ x: -1, y: 0 },{ x: -1, y: 1 },{ x: 0, y: 1 }].map(function(val){
+            return (parseInt(position.x) + parseInt(val.x)) + '_' + (parseInt(position.y) + parseInt(val.y));
+          });
+        */
+          options = (main.methods.options(startpoint, coordinates, main.variables.pieces[selectedpiece].type)).slice(0);
+          main.variables.highlighted = options.slice(0);
+          main.methods.togglehighlight(options);
+
+          break;
+        case 'w_queen':
+            
+            c1 = main.methods.b_options(position,[{x: 1, y: 1},{x: 2, y: 2},{x: 3, y: 3},{x: 4, y: 4},{x: 5, y: 5},{x: 6, y: 6},{x: 7, y: 7}]);
+            c2 = main.methods.b_options(position,[{x: 1, y: -1},{x: 2, y: -2},{x: 3, y: -3},{x: 4, y: -4},{x: 5, y: -5},{x: 6, y: -6},{x: 7, y: -7}]);
+            c3 = main.methods.b_options(position,[{x: -1, y: 1},{x: -2, y: 2},{x: -3, y: 3},{x: -4, y: 4},{x: -5, y: 5},{x: -6, y: 6},{x: -7, y: 7}]);
+            c4 = main.methods.b_options(position,[{x: -1, y: -1},{x: -2, y: -2},{x: -3, y: -3},{x: -4, y: -4},{x: -5, y: -5},{x: -6, y: -6},{x: -7, y: -7}]);
+            c5 = main.methods.b_options(position,[{x: 1, y: 0},{x: 2, y: 0},{x: 3, y: 0},{x: 4, y: 0},{x: 5, y: 0},{x: 6, y: 0},{x: 7, y: 0}]);
+            c6 = main.methods.b_options(position,[{x: 0, y: 1},{x: 0, y: 2},{x: 0, y: 3},{x: 0, y: 4},{x: 0, y: 5},{x: 0, y: 6},{x: 0, y: 7}]);
+            c7 = main.methods.b_options(position,[{x: -1, y: 0},{x: -2, y: 0},{x: -3, y: 0},{x: -4, y: 0},{x: -5, y: 0},{x: -6, y: 0},{x: -7, y: 0}]);
+            c8 = main.methods.b_options(position,[{x: 0, y: -1},{x: 0, y: -2},{x: 0, y: -3},{x: 0, y: -4},{x: 0, y: -5},{x: 0, y: -6},{x: 0, y: -7}]);
+        }
+    }
+}
+}
